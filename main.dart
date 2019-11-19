ab7e59aabcf8039d7006efe19092650ea8676a54
Ram <52283369+ABINASH56@users.noreply.github.com>
2019-11-19 13:41:10 +0530

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter_tempo/util/theme_notifier.dart';
+import 'package:flutter_tempo/values/theme.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'screens/home_page.dart';
+
+void main() {
+  SystemChrome.setEnabledSystemUIOverlays([SystemUiOverlay.bottom]).then((_) {
+    SharedPreferences.getInstance().then((prefs) {
+      var darkModeOn = prefs.getBool('darkMode') ?? true;
+      runApp(
+        ChangeNotifierProvider<ThemeNotifier>(
+          builder: (_) => ThemeNotifier(darkModeOn ? darkTheme : lightTheme),
+          child: MyApp(),
+        ),
+      );
+    });
+  });
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final themeNotifier = Provider.of<ThemeNotifier>(context);
+    return MaterialApp(
+      title: 'Chitr',
+      debugShowCheckedModeBanner: false,
+      theme: themeNotifier.getTheme(),
+      home: HomePage(),
+    );
+  }
+}
